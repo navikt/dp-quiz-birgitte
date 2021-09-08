ac62e76a4b264a6ee767d81f038d6ba4529da1c2
Geir André Lund <geir.andre.lund@nav.no>
2021-09-08 08:16:23 +0200

    Oppdaterer kotlin til 1.5.30, ktor til 1.6.3, kotliquery til 1.6.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,42 +1,9 @@
-plugins {
-    application
-    id(Shadow.shadow) version Shadow.version
-    kotlin("jvm") version "1.4.20"
-    id(Spotless.spotless) version Spotless.version
-}
+buildscript { repositories { mavenCentral() } }
 
-repositories {
-    jcenter()
-    maven("https://jitpack.io")
+plugins {
+    id("dagpenger.rapid-and-rivers")
 }
 
 application {
-    mainClassName = "no.nav.dagpenger.quiz.birgitte.AppKt"
-}
-
-dependencies {
-    implementation(RapidAndRivers)
-    implementation(Konfig.konfig)
-    implementation(Kotlin.Logging.kotlinLogging)
-
-    testImplementation(Mockk.mockk)
-    testImplementation(Junit5.engine)
-    testImplementation(Junit5.api)
-}
-
-spotless {
-    kotlin {
-        ktlint(Ktlint.version)
-    }
-    kotlinGradle {
-        target("*.gradle.kts", "buildSrc/**/*.kt*")
-        ktlint(Ktlint.version)
-    }
-}
-
-tasks.test {
-    useJUnitPlatform()
-    testLogging {
-        events("passed", "skipped", "failed")
-    }
+    mainClass.set("no.nav.dagpenger.quiz.birgitte.AppKt")
 }
